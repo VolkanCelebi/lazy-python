919efcbba1bf23415869d49d7f247eba4b821e50
VolkanCelebi <velebi@gmail.com>
2020-09-28 14:42:33 +0300

    Create not-operator-python.py

---
+++ not-operator-python.py
@@ -0,0 +1,13 @@
+#not operatörü kullanmayı seviyorum
+
+#Örneğin Aç-Kapa fonksiyonu yazıyor bizim junior python cılar
+def ac_kapa(self):
+    if self.toggled == false:
+        self.toggled = true
+    else:
+        self.toggled = false
+
+#Şimdi üşengeç python cıya bakalım    
+def ac_kapa_usengec(self):
+    self.toggled = not self.toggled
+
